4fd824436c3c6980d37146896a089674d42d454c
Leandro Robert <leanrobert24@gmail.com>
2023-04-04 14:58:29 -0300

    Theory updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,16 @@
 import { View } from "react-native"
 import RepositoryList from './RepositoryList'
 import AppBar from './AppBar'
+import { Navigate, Route, Routes } from "react-router-native"
 
 const Main = () => {
   return (
     <View style={{ backgroundColor: '#e1e4e8' }} >
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="*" element={<Navigate to='/' replace />} />
+      </Routes>
     </View>
   )
 }
