4b1621e89c23832d356c42d59abcf47a3b7c5703
Leandro Robert <leanrobert24@gmail.com>
2023-04-04 14:51:59 -0300

    Exercise 10.5: polished reviewed repositories list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,11 @@
 import { View } from "react-native"
 import RepositoryList from './RepositoryList'
-import Text from './Text'
 import AppBar from './AppBar'
 
 const Main = () => {
   return (
-    <View>
+    <View style={{ backgroundColor: '#e1e4e8' }} >
       <AppBar />
-      <Text fontWeight="bold" fontSize="subheading" color="textSecondary">Rate Repository Application</Text>
       <RepositoryList />
     </View>
   )
--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,16 +1,78 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+
+const styles = {
+  container: {
+    padding: 20,
+    backgroundColor: 'white'
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+  flexContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  flexContainer2: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    backgroundColor: '#0366d6',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    overflow: 'hidden',
+  },
+  flexContainer3: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+}
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View>
-      <Text>Full name: {item.fullName}</Text>
-      <Text>Description: {item.description}</Text>
-      <Text>Language: {item.language}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Fork:s {item.forksCount}</Text>
-      <Text>Reviews: {item.reviewCount}</Text>
-      <Text>Rating: {item.ratingAverage}</Text>
+    <View style={styles.container}>
+      <View style={styles.flexContainer}>
+        <View style={{ paddingRight: 20 }}>
+          <Image source={{ uri: item.ownerAvatarUrl }} style={styles.image} />
+        </View>
+        <View>
+          <Text style={{ fontWeight: 'bold' }}>{item.fullName}</Text>
+          <Text>{item.description}</Text>
+          <View style={{ flexWrap: 'wrap', marginVertical: 10}} >
+          <Text style={styles.label}>{item.language}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.flexContainer2}>
+        <View style={styles.flexContainer3}>
+          <Text style={{ fontWeight: 'bold' }}>{(item.stargazersCount / 1000).toFixed(1)}k</Text>
+          <Text>Stars</Text>
+        </View>
+        <View style={styles.flexContainer3}>
+          <Text style={{ fontWeight: 'bold' }}>{(item.forksCount / 1000).toFixed(1)}k</Text>
+          <Text>Forks</Text>
+        </View>
+        <View style={styles.flexContainer3}>
+          <Text style={{ fontWeight: 'bold' }}>{item.reviewCount}</Text>
+          <Text>Reviews</Text>
+        </View>
+        <View style={styles.flexContainer3}>
+          <Text style={{ fontWeight: 'bold' }}>{item.ratingAverage}</Text>
+          <Text>Rating</Text>
+        </View>
+      </View>
     </View>
   )
 }
