d2d10dad0ccb56fa3c356093fc24d6c19ce382c1
Leandro Robert <leanrobert24@gmail.com>
2023-04-05 13:26:45 -0300

    Exercise 10.16: sign out

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,7 +1,11 @@
 import { ScrollView, StyleSheet, View } from "react-native";
 import Constants from 'expo-constants';
 import Text from "./Text";
-import { Link } from "react-router-native";
+import { Link, useNavigate } from "react-router-native";
+import { ME } from "../graphql/queries";
+import { useApolloClient, useQuery } from "@apollo/client";
+import { useContext } from "react";
+import AuthStorageContext from "../contexts/AuthStorageContext";
 
 const styles = StyleSheet.create({
   container: {
@@ -17,15 +21,35 @@ const styles = StyleSheet.create({
 })
 
 const AppBar = () => {
+  const { data } = useQuery(ME);
+  const navigate = useNavigate();
+
+  console.log(data);
+
+  const authStorage = useContext(AuthStorageContext);
+  const apolloClient = useApolloClient();
+
+  const logout = async () => {
+    await authStorage.removeAccessToken();
+    apolloClient.resetStore();
+    navigate('/signin')
+  }
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
         <Link to='/'>
           <Text style={styles.textContainer}>Repositories</Text>
         </Link>
-        <Link to='/signin'>
-          <Text style={styles.textContainer}>Sign In</Text>
-        </Link>
+        {data?.me ? (
+          <Link to='/' onPress={logout}>
+            <Text style={styles.textContainer}>Sign Out</Text>
+          </Link>
+        ) : (
+          <Link to='/signin'>
+            <Text style={styles.textContainer}>Sign In</Text>
+          </Link>
+        )}
       </ScrollView>
     </View>
   )
